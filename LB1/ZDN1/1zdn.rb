6267fbf30a5549f6f0cab19d11d71ef8fd1e13de
hallucination <68617430+llliqwi@users.noreply.github.com>
2022-02-17 16:16:52 +0300

    Add files via upload

---
+++ LB1/ZDN1/1zdn.rb
@@ -0,0 +1,28 @@
+puts "Hello, wordl"
+#комментарий
+puts "Hello,"+ ARGV[0]
+puts "Какой у вас любимый язык," + ARGV[0] + "?"
+langprog = STDIN.gets.chomp
+case langprog
+when "ruby"
+  puts "подлиза"
+when "Python"
+  puts "Питонист zdddd"
+when "C++"
+  puts "Тебе делать нечего?"
+when "R"
+  puts "BigDATA???"
+else
+  puts "Такого нет"
+end
+
+puts "ВВедите команду ruby или OC"
+my_chose = STDIN.gets.chomp
+case my_chose
+when "ruby"
+  puts system "#{STDIN.gets.chomp}"
+when "OC"
+  puts system "#{STDIN.gets.chomp}"
+else
+  puts "Error"
+end
